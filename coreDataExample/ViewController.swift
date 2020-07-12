5a86e53c108e2fd70c8ad781c07b51bb0d2c9afb
Wdp <wdpmobileteam@gmail.com>
2020-07-12 11:17:56 +0530

    Initial Commit

---
+++ coreDataExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreDataExample
+//
+//  Created by WDP on 12/07/20.
+//  Copyright © 2020 WDP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
